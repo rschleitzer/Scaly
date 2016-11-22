ab763b5f8e76c89653bd735cd1e262be90a25abe
Ralf <Ralf.Schleitzer@gmx.de>
2016-11-22 21:32:17 +0100

    added shortest sources

---
+++ shortest/shortest.cpp
@@ -0,0 +1,12 @@
+#include "shortest.h"
+using namespace scaly;
+namespace shortest {
+
+int _main(_Vector<string>* args) {
+_Region _rp; _Page* _p = _rp.get();
+
+return 0;
+
+}
+
+}
---
+++ shortest/shortest.h
@@ -0,0 +1,12 @@
+#ifndef __scaly__shortest__
+#define __scaly__shortest__
+
+#include "Scaly.h"
+#include "shortest.h"
+
+using namespace scaly;
+namespace shortest {
+int _main(_Vector<string>* arguments);
+}
+
+#endif // __scaly__shortest__
