52fb81ae75f70d6e1a9fbac30203439c2caefe9f
Ralf <Ralf.Schleitzer@gmx.de>
2016-11-27 20:09:30 +0100

    thrown out ParserError

--- scalyc/CompilerError.cpp
+++ scalyc/CompilerError.cpp
@@ -2,6 +2,63 @@
 using namespace scaly;
 namespace scalyc {
 
+long ParserError::_getErrorCode() {
+    return (long)errorCode;
+}
+
+void* ParserError::_getErrorInfo() {
+    return errorInfo;
+}
+
+_ParserError_identifierExpected::_ParserError_identifierExpected(Position* position) 
+: position(position) { }
+
+_ParserError_identifierExpected* ParserError::get_identifierExpected() {
+    return (_ParserError_identifierExpected*)errorInfo;
+}
+
+_ParserError_literalExpected::_ParserError_literalExpected(Position* position) 
+: position(position) { }
+
+_ParserError_literalExpected* ParserError::get_literalExpected() {
+    return (_ParserError_literalExpected*)errorInfo;
+}
+
+_ParserError_keywordExpected::_ParserError_keywordExpected(Position* position, string* keyword) 
+: position(position), keyword(keyword) { }
+
+_ParserError_keywordExpected* ParserError::get_keywordExpected() {
+    return (_ParserError_keywordExpected*)errorInfo;
+}
+
+_ParserError_punctuationExpected::_ParserError_punctuationExpected(Position* position, string* punctuation) 
+: position(position), punctuation(punctuation) { }
+
+_ParserError_punctuationExpected* ParserError::get_punctuationExpected() {
+    return (_ParserError_punctuationExpected*)errorInfo;
+}
+
+_ParserError_operatorExpected::_ParserError_operatorExpected(Position* position) 
+: position(position) { }
+
+_ParserError_operatorExpected* ParserError::get_operatorExpected() {
+    return (_ParserError_operatorExpected*)errorInfo;
+}
+
+_ParserError_unableToParse::_ParserError_unableToParse(Position* position, _Array<ParserError>* errors) 
+: position(position), errors(errors) { }
+
+_ParserError_unableToParse* ParserError::get_unableToParse() {
+    return (_ParserError_unableToParse*)errorInfo;
+}
+
+_ParserError_notAtEnd::_ParserError_notAtEnd(Position* position) 
+: position(position) { }
+
+_ParserError_notAtEnd* ParserError::get_notAtEnd() {
+    return (_ParserError_notAtEnd*)errorInfo;
+}
+
 long CompilerError::_getErrorCode() {
     return (long)errorCode;
 }
@@ -52,6 +109,5 @@ _CppError_unableToCreateOutputDirectory::_CppError_unableToCreateOutputDirectory
 _CppError_unableToCreateOutputDirectory* CppError::get_unableToCreateOutputDirectory() {
     return (_CppError_unableToCreateOutputDirectory*)errorInfo;
 }
-
 
 }
--- scalyc/CompilerError.h
+++ scalyc/CompilerError.h
@@ -4,6 +4,111 @@
 using namespace scaly;
 namespace scalyc {
 
+class ParserError;
+
+class _ParserError_identifierExpected : public Object {
+public:
+    _ParserError_identifierExpected(Position* position);
+
+    Position* position;
+};
+
+class _ParserError_literalExpected : public Object {
+public:
+    _ParserError_literalExpected(Position* position);
+
+    Position* position;
+};
+
+class _ParserError_keywordExpected : public Object {
+public:
+    _ParserError_keywordExpected(Position* position, string* keyword);
+
+    Position* position;
+    string* keyword;
+};
+
+class _ParserError_punctuationExpected : public Object {
+public:
+    _ParserError_punctuationExpected(Position* position, string* punctuation);
+
+    Position* position;
+    string* punctuation;
+};
+
+class _ParserError_operatorExpected : public Object {
+public:
+    _ParserError_operatorExpected(Position* position);
+
+    Position* position;
+};
+
+class _ParserError_unableToParse : public Object {
+public:
+    _ParserError_unableToParse(Position* position, _Array<ParserError>* errors);
+
+    Position* position;
+    _Array<ParserError>* errors;
+};
+
+class _ParserError_notAtEnd : public Object {
+public:
+    _ParserError_notAtEnd(Position* position);
+
+    Position* position;
+};
+enum _ParserErrorCode {
+    _ParserErrorCode_identifierExpected = 1,
+    _ParserErrorCode_literalExpected,
+    _ParserErrorCode_keywordExpected,
+    _ParserErrorCode_punctuationExpected,
+    _ParserErrorCode_operatorExpected,
+    _ParserErrorCode_unableToParse,
+    _ParserErrorCode_notAtEnd,
+};
+
+class ParserError : public Object {
+public:
+    ParserError(_ParserErrorCode errorCode)
+    : errorCode(errorCode), errorInfo(0) {}
+
+    ParserError(_ParserError_identifierExpected* identifierExpected)
+    : errorCode(_ParserErrorCode_identifierExpected), errorInfo(identifierExpected) {}
+
+    ParserError(_ParserError_literalExpected* literalExpected)
+    : errorCode(_ParserErrorCode_literalExpected), errorInfo(literalExpected) {}
+
+    ParserError(_ParserError_keywordExpected* keywordExpected)
+    : errorCode(_ParserErrorCode_keywordExpected), errorInfo(keywordExpected) {}
+
+    ParserError(_ParserError_punctuationExpected* punctuationExpected)
+    : errorCode(_ParserErrorCode_punctuationExpected), errorInfo(punctuationExpected) {}
+
+    ParserError(_ParserError_operatorExpected* operatorExpected)
+    : errorCode(_ParserErrorCode_operatorExpected), errorInfo(operatorExpected) {}
+
+    ParserError(_ParserError_unableToParse* unableToParse)
+    : errorCode(_ParserErrorCode_unableToParse), errorInfo(unableToParse) {}
+
+    ParserError(_ParserError_notAtEnd* notAtEnd)
+    : errorCode(_ParserErrorCode_notAtEnd), errorInfo(notAtEnd) {}
+
+    long _getErrorCode();
+    void* _getErrorInfo();
+
+    _ParserError_identifierExpected* get_identifierExpected();
+    _ParserError_literalExpected* get_literalExpected();
+    _ParserError_keywordExpected* get_keywordExpected();
+    _ParserError_punctuationExpected* get_punctuationExpected();
+    _ParserError_operatorExpected* get_operatorExpected();
+    _ParserError_unableToParse* get_unableToParse();
+    _ParserError_notAtEnd* get_notAtEnd();
+
+private:
+    _ParserErrorCode errorCode;
+    void* errorInfo;
+};
+
 class CompilerError;
 
 class _CompilerError_fileNotFound : public Object {
