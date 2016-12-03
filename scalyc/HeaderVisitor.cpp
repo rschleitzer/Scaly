9bf161ac41e2863d3af87c19a85a13e98c9d884d
Ralf <Ralf.Schleitzer@gmx.de>
2016-12-03 14:16:32 +0100

    refactoring

--- scalyc/HeaderVisitor.cpp
+++ scalyc/HeaderVisitor.cpp
@@ -54,9 +54,6 @@ bool HeaderVisitor::openProgram(Program* program) {
     return true;
 }
 
-void HeaderVisitor::closeProgram(Program* program) {
-}
-
 bool HeaderVisitor::openCompilationUnit(CompilationUnit* compilationUnit) {
     moduleName = compilationUnit->fileName;
     headerIndentLevel = 0;
@@ -121,9 +118,6 @@ bool HeaderVisitor::openConstantDeclaration(ConstantDeclaration* constantDeclara
     return false;
 }
 
-void HeaderVisitor::closeConstantDeclaration(ConstantDeclaration* constantDeclaration) {
-}
-
 bool HeaderVisitor::openVariableDeclaration(VariableDeclaration* variableDeclaration) {
     if (variableDeclaration->parent->parent->parent == nullptr)
         return false;
@@ -132,18 +126,12 @@ bool HeaderVisitor::openVariableDeclaration(VariableDeclaration* variableDeclara
     return false;
 }
 
-void HeaderVisitor::closeVariableDeclaration(VariableDeclaration* variableDeclaration) {
-}
-
 bool HeaderVisitor::openMutableDeclaration(MutableDeclaration* mutableDeclaration) {
     if (mutableDeclaration->parent->parent->parent == nullptr)
         return false;
     if (mutableDeclaration->parent->parent->parent->_isClassDeclaration())
         return true;
     return false;
-}
-
-void HeaderVisitor::closeMutableDeclaration(MutableDeclaration* mutableDeclaration) {
 }
 
 bool HeaderVisitor::openFunctionDeclaration(FunctionDeclaration* functionDeclaration) {
@@ -329,36 +317,13 @@ bool HeaderVisitor::openConstructorDeclaration(ConstructorDeclaration* construct
     return true;
 }
 
-void HeaderVisitor::closeConstructorDeclaration(ConstructorDeclaration* constructorDeclaration) {
-}
-
 bool HeaderVisitor::openCodeBlock(CodeBlock* codeBlock) {
     return false;
-}
-
-void HeaderVisitor::closeCodeBlock(CodeBlock* codeBlock) {
-}
-
-bool HeaderVisitor::openSimpleExpression(SimpleExpression* simpleExpression) {
-    return true;
-}
-
-void HeaderVisitor::closeSimpleExpression(SimpleExpression* simpleExpression) {
-}
-
-bool HeaderVisitor::openInitializer(Initializer* initializer) {
-    return true;
-}
-
-void HeaderVisitor::closeInitializer(Initializer* initializer) {
 }
 
 bool HeaderVisitor::openBindingInitializer(BindingInitializer* bindingInitializer) {
     firstBindingInitializer = true;
     return true;
-}
-
-void HeaderVisitor::closeBindingInitializer(BindingInitializer* bindingInitializer) {
 }
 
 bool HeaderVisitor::openPatternInitializer(PatternInitializer* patternInitializer) {
@@ -367,22 +332,6 @@ bool HeaderVisitor::openPatternInitializer(PatternInitializer* patternInitialize
     else
         firstBindingInitializer = false;
     return true;
-}
-
-void HeaderVisitor::closePatternInitializer(PatternInitializer* patternInitializer) {
-}
-
-bool HeaderVisitor::openAdditionalInitializer(AdditionalInitializer* additionalInitializer) {
-    return true;
-}
-
-void HeaderVisitor::closeAdditionalInitializer(AdditionalInitializer* additionalInitializer) {
-}
-
-void HeaderVisitor::visitOverrideWord(OverrideWord* overrideWord) {
-}
-
-void HeaderVisitor::visitStaticWord(StaticWord* staticWord) {
 }
 
 bool HeaderVisitor::openFunctionSignature(FunctionSignature* functionSignature) {
@@ -456,16 +405,6 @@ bool HeaderVisitor::openFunctionSignature(FunctionSignature* functionSignature) 
     return true;
 }
 
-void HeaderVisitor::closeFunctionSignature(FunctionSignature* functionSignature) {
-}
-
-bool HeaderVisitor::openFunctionResult(FunctionResult* functionResult) {
-    return true;
-}
-
-void HeaderVisitor::closeFunctionResult(FunctionResult* functionResult) {
-}
-
 bool HeaderVisitor::openParameterClause(ParameterClause* parameterClause) {
     firstParameter = true;
     return true;
@@ -524,13 +463,6 @@ void HeaderVisitor::closeVarParameter(VarParameter* varParameter) {
     headerFile->append(varParameterName);
 }
 
-bool HeaderVisitor::openThrowsClause(ThrowsClause* throwsClause) {
-    return true;
-}
-
-void HeaderVisitor::closeThrowsClause(ThrowsClause* throwsClause) {
-}
-
 bool HeaderVisitor::openEnumMember(EnumMember* enumMember) {
     if (!enumMember->parent->_isEnumDeclaration())
         return false;
@@ -576,23 +508,6 @@ void HeaderVisitor::closeEnumMember(EnumMember* enumMember) {
     }
 }
 
-void HeaderVisitor::visitEnumCase(EnumCase* enumCase) {
-}
-
-bool HeaderVisitor::openAdditionalCase(AdditionalCase* additionalCase) {
-    return true;
-}
-
-void HeaderVisitor::closeAdditionalCase(AdditionalCase* additionalCase) {
-}
-
-bool HeaderVisitor::openClassBody(ClassBody* classBody) {
-    return true;
-}
-
-void HeaderVisitor::closeClassBody(ClassBody* classBody) {
-}
-
 void HeaderVisitor::indentHeader() {
     size_t i = 0;
     while (i < headerIndentLevel) {
@@ -635,226 +550,6 @@ void HeaderVisitor::closeClassMember(ClassMember* classMember) {
     headerFile->append(";");
 }
 
-bool HeaderVisitor::openPrefixExpression(PrefixExpression* prefixExpression) {
-    return true;
-}
-
-void HeaderVisitor::closePrefixExpression(PrefixExpression* prefixExpression) {
-}
-
-bool HeaderVisitor::openPostfixExpression(PostfixExpression* postfixExpression) {
-    return true;
-}
-
-void HeaderVisitor::closePostfixExpression(PostfixExpression* postfixExpression) {
-}
-
-bool HeaderVisitor::openBinaryOperation(BinaryOperation* binaryOperation) {
-    return true;
-}
-
-void HeaderVisitor::closeBinaryOperation(BinaryOperation* binaryOperation) {
-}
-
-bool HeaderVisitor::openAssignment(Assignment* assignment) {
-    return true;
-}
-
-void HeaderVisitor::closeAssignment(Assignment* assignment) {
-}
-
-bool HeaderVisitor::openTypeQuery(TypeQuery* typeQuery) {
-    return true;
-}
-
-void HeaderVisitor::closeTypeQuery(TypeQuery* typeQuery) {
-}
-
-bool HeaderVisitor::openTypeCast(TypeCast* typeCast) {
-    return true;
-}
-
-void HeaderVisitor::closeTypeCast(TypeCast* typeCast) {
-}
-
-bool HeaderVisitor::openCatchClause(CatchClause* catchClause) {
-    return true;
-}
-
-void HeaderVisitor::closeCatchClause(CatchClause* catchClause) {
-}
-
-bool HeaderVisitor::openWildCardCatchPattern(WildCardCatchPattern* wildCardCatchPattern) {
-    return true;
-}
-
-void HeaderVisitor::closeWildCardCatchPattern(WildCardCatchPattern* wildCardCatchPattern) {
-}
-
-bool HeaderVisitor::openIdentifierCatchPattern(IdentifierCatchPattern* identifierCatchPattern) {
-    return true;
-}
-
-void HeaderVisitor::closeIdentifierCatchPattern(IdentifierCatchPattern* identifierCatchPattern) {
-}
-
-void HeaderVisitor::visitOperatorPostfix(OperatorPostfix* operatorPostfix) {
-}
-
-bool HeaderVisitor::openFunctionCall(FunctionCall* functionCall) {
-    return true;
-}
-
-void HeaderVisitor::closeFunctionCall(FunctionCall* functionCall) {
-}
-
-void HeaderVisitor::visitMemberExpression(MemberExpression* memberExpression) {
-}
-
-bool HeaderVisitor::openSubscript(Subscript* subscript) {
-    return true;
-}
-
-void HeaderVisitor::closeSubscript(Subscript* subscript) {
-}
-
-bool HeaderVisitor::openExpressionElement(ExpressionElement* expressionElement) {
-    return true;
-}
-
-void HeaderVisitor::closeExpressionElement(ExpressionElement* expressionElement) {
-}
-
-void HeaderVisitor::visitIdentifierExpression(IdentifierExpression* identifierExpression) {
-}
-
-void HeaderVisitor::visitLiteralExpression(LiteralExpression* literalExpression) {
-}
-
-bool HeaderVisitor::openIfExpression(IfExpression* ifExpression) {
-    return true;
-}
-
-void HeaderVisitor::closeIfExpression(IfExpression* ifExpression) {
-}
-
-bool HeaderVisitor::openSwitchExpression(SwitchExpression* switchExpression) {
-    return true;
-}
-
-void HeaderVisitor::closeSwitchExpression(SwitchExpression* switchExpression) {
-}
-
-bool HeaderVisitor::openForExpression(ForExpression* forExpression) {
-    return true;
-}
-
-void HeaderVisitor::closeForExpression(ForExpression* forExpression) {
-}
-
-bool HeaderVisitor::openWhileExpression(WhileExpression* whileExpression) {
-    return true;
-}
-
-void HeaderVisitor::closeWhileExpression(WhileExpression* whileExpression) {
-}
-
-bool HeaderVisitor::openDoExpression(DoExpression* doExpression) {
-    return true;
-}
-
-void HeaderVisitor::closeDoExpression(DoExpression* doExpression) {
-}
-
-bool HeaderVisitor::openParenthesizedExpression(ParenthesizedExpression* parenthesizedExpression) {
-    return true;
-}
-
-void HeaderVisitor::closeParenthesizedExpression(ParenthesizedExpression* parenthesizedExpression) {
-}
-
-bool HeaderVisitor::openReturnExpression(ReturnExpression* returnExpression) {
-    return true;
-}
-
-void HeaderVisitor::closeReturnExpression(ReturnExpression* returnExpression) {
-}
-
-bool HeaderVisitor::openThrowExpression(ThrowExpression* throwExpression) {
-    return true;
-}
-
-void HeaderVisitor::closeThrowExpression(ThrowExpression* throwExpression) {
-}
-
-bool HeaderVisitor::openBreakExpression(BreakExpression* breakExpression) {
-    return true;
-}
-
-void HeaderVisitor::closeBreakExpression(BreakExpression* breakExpression) {
-}
-
-bool HeaderVisitor::openConstructorCall(ConstructorCall* constructorCall) {
-    return true;
-}
-
-void HeaderVisitor::closeConstructorCall(ConstructorCall* constructorCall) {
-}
-
-void HeaderVisitor::visitThisExpression(ThisExpression* thisExpression) {
-}
-
-void HeaderVisitor::visitNullExpression(NullExpression* nullExpression) {
-}
-
-bool HeaderVisitor::openElseClause(ElseClause* elseClause) {
-    return true;
-}
-
-void HeaderVisitor::closeElseClause(ElseClause* elseClause) {
-}
-
-bool HeaderVisitor::openCurliedSwitchBody(CurliedSwitchBody* curliedSwitchBody) {
-    return true;
-}
-
-void HeaderVisitor::closeCurliedSwitchBody(CurliedSwitchBody* curliedSwitchBody) {
-}
-
-bool HeaderVisitor::openNakedSwitchBody(NakedSwitchBody* nakedSwitchBody) {
-    return true;
-}
-
-void HeaderVisitor::closeNakedSwitchBody(NakedSwitchBody* nakedSwitchBody) {
-}
-
-bool HeaderVisitor::openSwitchCase(SwitchCase* switchCase) {
-    return true;
-}
-
-void HeaderVisitor::closeSwitchCase(SwitchCase* switchCase) {
-}
-
-bool HeaderVisitor::openItemCaseLabel(ItemCaseLabel* itemCaseLabel) {
-    return true;
-}
-
-void HeaderVisitor::closeItemCaseLabel(ItemCaseLabel* itemCaseLabel) {
-}
-
-void HeaderVisitor::visitDefaultCaseLabel(DefaultCaseLabel* defaultCaseLabel) {
-}
-
-bool HeaderVisitor::openCaseItem(CaseItem* caseItem) {
-    return true;
-}
-
-void HeaderVisitor::closeCaseItem(CaseItem* caseItem) {
-}
-
-void HeaderVisitor::visitWildcardPattern(WildcardPattern* wildcardPattern) {
-}
-
 bool HeaderVisitor::openIdentifierPattern(IdentifierPattern* identifierPattern) {
     if (identifierPattern->annotationForType != nullptr) {
         identifierPattern->annotationForType->accept(this);
@@ -862,37 +557,6 @@ bool HeaderVisitor::openIdentifierPattern(IdentifierPattern* identifierPattern) 
     }
     headerFile->append(identifierPattern->identifier);
     return false;
-}
-
-void HeaderVisitor::closeIdentifierPattern(IdentifierPattern* identifierPattern) {
-}
-
-bool HeaderVisitor::openTuplePattern(TuplePattern* tuplePattern) {
-    return true;
-}
-
-void HeaderVisitor::closeTuplePattern(TuplePattern* tuplePattern) {
-}
-
-bool HeaderVisitor::openExpressionPattern(ExpressionPattern* expressionPattern) {
-    return true;
-}
-
-void HeaderVisitor::closeExpressionPattern(ExpressionPattern* expressionPattern) {
-}
-
-bool HeaderVisitor::openTuplePatternElement(TuplePatternElement* tuplePatternElement) {
-    return true;
-}
-
-void HeaderVisitor::closeTuplePatternElement(TuplePatternElement* tuplePatternElement) {
-}
-
-bool HeaderVisitor::openCaseContent(CaseContent* caseContent) {
-    return true;
-}
-
-void HeaderVisitor::closeCaseContent(CaseContent* caseContent) {
 }
 
 bool HeaderVisitor::openType(Type* type) {
@@ -911,56 +575,6 @@ void HeaderVisitor::closeType(Type* type) {
     if (isClass(type->name) && !hasArrayPostfix(type) && !type->parent->_isConstructorCall()) {
         headerFile->append("*");
     }
-}
-
-bool HeaderVisitor::openTypeAnnotation(TypeAnnotation* typeAnnotation) {
-    return true;
-}
-
-void HeaderVisitor::closeTypeAnnotation(TypeAnnotation* typeAnnotation) {
-}
-
-bool HeaderVisitor::openSubtype(Subtype* subtype) {
-    return true;
-}
-
-void HeaderVisitor::closeSubtype(Subtype* subtype) {
-}
-
-bool HeaderVisitor::openIndexedType(IndexedType* indexedType) {
-    return true;
-}
-
-void HeaderVisitor::closeIndexedType(IndexedType* indexedType) {
-}
-
-void HeaderVisitor::visitPointer(Pointer* pointer) {
-}
-
-void HeaderVisitor::visitRoot(Root* root) {
-}
-
-void HeaderVisitor::visitLocal(Local* local) {
-}
-
-void HeaderVisitor::visitReference(Reference* reference) {
-}
-
-void HeaderVisitor::visitThrown(Thrown* thrown) {
-}
-
-bool HeaderVisitor::openTypeInheritanceClause(TypeInheritanceClause* typeInheritanceClause) {
-    return true;
-}
-
-void HeaderVisitor::closeTypeInheritanceClause(TypeInheritanceClause* typeInheritanceClause) {
-}
-
-bool HeaderVisitor::openInheritance(Inheritance* inheritance) {
-    return true;
-}
-
-void HeaderVisitor::closeInheritance(Inheritance* inheritance) {
 }
 
 void HeaderVisitor::buildMainHeaderFileString(Program* program) {
