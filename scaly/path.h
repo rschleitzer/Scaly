8a835ad37380056e3b7e4756c8a271c87ea3aee7
rschleitzer <ralf.schleitzer@gmx.de>
2017-11-23 07:13:08 +0100

    deleted c

--- scaly/path.h
+++
@@ -1,7 +0,0 @@
-#ifndef __scaly__path__
-#define __scaly__path__
-
-scaly_string* scaly_Path_getFileNameWithoutExtension(scaly_Page* _rp, scaly_string* path);
-scaly_string* scaly_Path_getFileName(scaly_Page* _rp, scaly_string* path);
-
-#endif // __Scaly__Path__
--- scalyc/compiler.h
+++
@@ -1,8 +0,0 @@
-#ifndef __scalyc__compiler__
-#define __scalyc__compiler__
-#include "scalyc.h"
-
-void scalyc_Compiler_compileFiles(scalyc_Options* options);
-_scalyc_Compiler_Result parseUnit(scaly_Page* _rp, scaly_Page* _ep, scaly_string* moduleName, scaly_string* text);
-
-#endif // __scalyc__compiler__
