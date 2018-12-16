6c287daa52af9f00dfa31bcb5e99284f328b9a55
rschleitzer <ralf.schleitzer@gmx.de>
2018-12-16 06:11:49 -0800

    Empty scaly library 0.0.6

---
+++ scaly/target/debug/deps/scaly-eda067d8c4185e82.d
@@ -0,0 +1,5 @@
+/home/ralf/Scaly/scaly/target/debug/deps/libscaly-eda067d8c4185e82.rlib: src/lib.rs
+
+/home/ralf/Scaly/scaly/target/debug/deps/scaly-eda067d8c4185e82.d: src/lib.rs
+
+src/lib.rs:
---
+++ scaly/target/debug/libscaly.d
@@ -0,0 +1,1 @@
+/home/ralf/Scaly/scaly/target/debug/libscaly.rlib: /home/ralf/Scaly/scaly/src/lib.rs
