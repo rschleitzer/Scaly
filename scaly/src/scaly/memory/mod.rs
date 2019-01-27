abb1b3bf4c71ef969408678819e51707a6e4ad1b
rschleitzer <ralf.schleitzer@gmx.de>
2019-01-27 04:02:29 -0800

    Dropped the idea of a context, prepared for stack bucket

--- scaly/src/scaly/memory/mod.rs
+++ scaly/src/scaly/memory/mod.rs
@@ -1,7 +1,5 @@
 mod page;
-mod context;
 mod region;
 
 pub use self::page::_Page;
-pub use self::context::_Context;
 pub use self::region::_Region;
--- scaly/src/scaly/memory/region.rs
+++ scaly/src/scaly/memory/region.rs
@@ -1,21 +1,29 @@
 use scaly::memory::page::_Page;
-use scaly::memory::context::_Context;
+use scaly::memory::page::_PAGE_SIZE;
+
+pub const _STACK_PAGES: isize = 0x100;
 
 pub struct _Region<'a> {
-    context: &'a mut _Context,
+    page: &'a mut _Page,
 }
 
 impl<'a> _Region<'a> {
-    pub fn create(context: &mut _Context) -> _Region {
+    pub fn create(page: & _Page) -> _Region<'a> {
+        let our_page_address = page as *const _Page as usize + _PAGE_SIZE;
+        let base_stack_bucket_address = our_page_address & !(_PAGE_SIZE * _STACK_PAGES as usize - 1);
+        let overflowed_page_address = base_stack_bucket_address + _PAGE_SIZE * _STACK_PAGES as usize;
+        if overflowed_page_address == our_page_address {
+            panic!("Page stack overflow.");
+        }
         _Region {
-            context: context,
+            page: unsafe { &mut *(our_page_address as *mut _Page) }
         }
     }
 }
 
 impl<'a>  Drop for  _Region<'a> {
     fn drop(&mut self) {
-        self.context.deallocate_page();
+        self.page.deallocate_extensions();
     }
 }
 
@@ -25,19 +33,17 @@ fn test_region() {
     use std::alloc::alloc;
     use std::alloc::Layout;
     use scaly::memory::page::_PAGE_SIZE;
-    use scaly::memory::context::_STACK_PAGES;
 
     unsafe {
         let memory = alloc(Layout::from_size_align_unchecked(_PAGE_SIZE * _STACK_PAGES as usize, _PAGE_SIZE));
         assert_ne!(memory, null_mut());
-        let mut context = _Context::initialize(memory as *mut _Page);
-        region_function(&mut context);
-    }
-}
-
-fn region_function(_c: &mut _Context) {
-    let mut r = _Region::create(_c);
-    {
-        
+        let r1 = _Region::create(& *(memory as *const _Page));
+        {
+            let r2 = _Region::create(r1.page);
+            {
+                let _ = _Region::create(r2.page);
+                
+            }
+        }
     }
 }
