bdc341dc80b8095fd954d71ca62abab3d62958f2
rschleitzer <ralf.schleitzer@gmx.de>
2019-02-06 12:22:25 -0800

    Fixed HeapBucket de-allocation

--- scaly/src/scaly/memory/heapbucket.rs
+++ scaly/src/scaly/memory/heapbucket.rs
@@ -1,10 +1,10 @@
 use scaly::memory::bucket::Bucket;
 use scaly::memory::bucket::Bucket::Heap;
 use scaly::memory::bucket::Bucket::Stack;
+use scaly::memory::bucket::BUCKET_PAGES;
 use scaly::memory::page::Page;
 use scaly::memory::page::PAGE_SIZE;
 use scaly::memory::pool::Pool;
-use std::mem::size_of;
 use std::usize::MAX;
 
 pub struct HeapBucket {
@@ -36,13 +36,13 @@ impl HeapBucket {
         if self.map == MAX {
             self.map = self.map - 1;
             let page = Page::get(self as *const HeapBucket as usize);
-            println!("HeapBucket allocated first page:{:X}", page as usize);
+            // println!("HeapBucket allocated first page:{:X}", page as usize);
             return page;
         }
 
         if self.map == 0 {
             unsafe {
-                //println!("self.pool: {:X}", self.pool as usize);
+                // println!("self.pool: {:X}", self.pool as usize);
                 if self as *mut HeapBucket == (*self.pool).current_bucket {
                     //println!("Let the pool allocate a new one.");
                     (*self.pool).allocate_bucket();
@@ -52,40 +52,44 @@ impl HeapBucket {
         }
 
         let position = HeapBucket::find_least_position(self.map);
-        println!("HeapBucket position: {}", position);
-        self.map = self.map & !(1 << size_of::<usize>() * 8 - position);
+        // println!("HeapBucket position: {}", position);
+        let bit = 1 << (BUCKET_PAGES - position);
+        // println!("Bit for allocation: {:X}", bit);
+        self.map = self.map & !bit;
 
         let page = (Page::get(self as *const HeapBucket as usize) as usize + position * PAGE_SIZE)
             as *mut Page;
-        println!("HeapBucket allocated page:{:X}", page as usize);
+        // println!("HeapBucket allocated page:{:X}", page as usize);
         unsafe { (*page).reset() }
         page
     }
 
     pub fn deallocate_page(&mut self, page: &Page) {
         let base_page = Page::get(self as *const HeapBucket as usize) as usize;
-        println!("HeapBucket about to de-allocate a page:{:X}", base_page);
+        // println!("HeapBucket about to de-allocate a page:{:X}", base_page);
         let distance = page as *const Page as usize - base_page;
         let position = distance / PAGE_SIZE;
-        println!("Position to de-allocate: {}", position);
-        if position > size_of::<usize>() * 8 - 1 {
+        // println!("Position to de-allocate: {}", position);
+        if position > BUCKET_PAGES - 1 {
             panic!("Position invalid for page {:X}", base_page)
         };
-        let bit = 1 << size_of::<usize>() * 8 - position - 1;
-        println!("Bit: {:X}", bit);
+        let bit = 1 << (BUCKET_PAGES - position);
+        // println!("Bit for deallocation: {:X}", bit);
         if self.map & bit != 0 {
             panic!("Page {:X} was not allocated!", base_page)
         }
-        self.map = self.map & bit;
+        // println!("self.map before deallocation: {:X}", self.map);
+        self.map = self.map | bit;
+        // println!("self.map after deallocation: {:X}", self.map);
     }
 
     fn find_least_position(n: usize) -> usize {
-        match size_of::<usize>() * 8 {
+        match BUCKET_PAGES {
             64 => HeapBucket::find_least_position_64(n),
             32 => HeapBucket::find_least_position_32(n),
             _ => panic!(
                 "Scaly currently does not support {}bit architecture.",
-                size_of::<usize>() * 8
+                BUCKET_PAGES
             ),
         }
     }
@@ -108,7 +112,8 @@ impl HeapBucket {
         let product = n as u128 * 0x03f6eaf2cd271461;
 
         let log = NUMBERS[product as usize >> 58];
-        size_of::<usize>() * 8 - log
+        // println!("Log: {}", log);
+        BUCKET_PAGES - log
     }
 
     // from http://graphics.stanford.edu/~seander/bithacks.html#IntegerLogDeBruijn
@@ -125,7 +130,7 @@ impl HeapBucket {
         let n = n | n >> 16;
 
         let log = NUMBERS[(n * 0x07C4ACDD) >> 27];
-        size_of::<usize>() * 8 - log
+        BUCKET_PAGES - log
     }
 }
 
@@ -135,26 +140,98 @@ fn test_heapbucket() {
         let mut pool = Pool::create();
         //println!("pool: {:X}", &pool as *const Pool as usize);
         let bucket = HeapBucket::create(&mut pool);
-        let page0 = (*bucket).allocate_page();
-        let page1 = (*bucket).allocate_page();
-        let page2 = (*bucket).allocate_page();
-        let page3 = (*bucket).allocate_page();
-        let page4 = (*bucket).allocate_page();
-        let page5 = (*bucket).allocate_page();
-        let page6 = (*bucket).allocate_page();
-        let page7 = (*bucket).allocate_page();
-        let page8 = (*bucket).allocate_page();
-        let page9 = (*bucket).allocate_page();
-        (*page1).forget();
-        (*page5).forget();
-        (*page4).forget();
-        (*page3).forget();
-        (*page8).forget();
-        (*page0).forget();
-        (*page7).forget();
-        (*page6).forget();
-        (*page2).forget();
-        (*page9).forget();
+        let _page00 = (*bucket).allocate_page();
+        let _page01 = (*bucket).allocate_page();
+        let page02 = (*bucket).allocate_page();
+        let page03 = (*bucket).allocate_page();
+        let page04 = (*bucket).allocate_page();
+        let page05 = (*bucket).allocate_page();
+        let _page06 = (*bucket).allocate_page();
+        let _page07 = (*bucket).allocate_page();
+        let _page08 = (*bucket).allocate_page();
+        let _page09 = (*bucket).allocate_page();
+        let _page10 = (*bucket).allocate_page();
+        let _page11 = (*bucket).allocate_page();
+        let _page12 = (*bucket).allocate_page();
+        let _page13 = (*bucket).allocate_page();
+        let _page14 = (*bucket).allocate_page();
+        let _page15 = (*bucket).allocate_page();
+        let page16 = (*bucket).allocate_page();
+        let _page17 = (*bucket).allocate_page();
+        let page18 = (*bucket).allocate_page();
+        let _page19 = (*bucket).allocate_page();
+        let _page20 = (*bucket).allocate_page();
+        let _page21 = (*bucket).allocate_page();
+        let _page22 = (*bucket).allocate_page();
+        let _page23 = (*bucket).allocate_page();
+        let _page24 = (*bucket).allocate_page();
+        let _page25 = (*bucket).allocate_page();
+        let _page26 = (*bucket).allocate_page();
+        let _page27 = (*bucket).allocate_page();
+        let _page28 = (*bucket).allocate_page();
+        let _page29 = (*bucket).allocate_page();
+        let _page30 = (*bucket).allocate_page();
+        let _page31 = (*bucket).allocate_page();
+        let _page32 = (*bucket).allocate_page();
+        let _page33 = (*bucket).allocate_page();
+        let _page34 = (*bucket).allocate_page();
+        let _page35 = (*bucket).allocate_page();
+        let _page36 = (*bucket).allocate_page();
+        let _page37 = (*bucket).allocate_page();
+        let _page38 = (*bucket).allocate_page();
+        let _page39 = (*bucket).allocate_page();
+        let _page40 = (*bucket).allocate_page();
+        let _page41 = (*bucket).allocate_page();
+        let _page42 = (*bucket).allocate_page();
+        let _page43 = (*bucket).allocate_page();
+        let _page44 = (*bucket).allocate_page();
+        let _page45 = (*bucket).allocate_page();
+        let _page46 = (*bucket).allocate_page();
+        let _page47 = (*bucket).allocate_page();
+        let _page48 = (*bucket).allocate_page();
+        let _page49 = (*bucket).allocate_page();
+        let _page50 = (*bucket).allocate_page();
+        let _page51 = (*bucket).allocate_page();
+        let _page52 = (*bucket).allocate_page();
+        let _page53 = (*bucket).allocate_page();
+        let _page54 = (*bucket).allocate_page();
+        let _page55 = (*bucket).allocate_page();
+        let _page56 = (*bucket).allocate_page();
+        let _page57 = (*bucket).allocate_page();
+        let _page58 = (*bucket).allocate_page();
+        let _page59 = (*bucket).allocate_page();
+        let _page60 = (*bucket).allocate_page();
+        let _page61 = (*bucket).allocate_page();
+        let page62 = (*bucket).allocate_page();
+        let page63 = (*bucket).allocate_page();
+        assert_eq!((*bucket).map, 0);
+        (*bucket).deallocate_page(&*page62);
+        let page62a = (*bucket).allocate_page();
+        assert_eq!(page62, page62a);
+        assert_eq!((*bucket).map, 0);
+        (*bucket).deallocate_page(&*page02);
+        (*bucket).deallocate_page(&*page03);
+        (*bucket).deallocate_page(&*page04);
+        (*bucket).deallocate_page(&*page05);
+        let page02a = (*bucket).allocate_page();
+        let page03a = (*bucket).allocate_page();
+        let page04a = (*bucket).allocate_page();
+        let page05a = (*bucket).allocate_page();
+        assert_eq!(page02, page02a);
+        assert_eq!(page03, page03a);
+        assert_eq!(page04, page04a);
+        assert_eq!(page05, page05a);
+        assert_eq!((*bucket).map, 0);
+        (*bucket).deallocate_page(&*page16);
+        (*bucket).deallocate_page(&*page18);
+        let page16a = (*bucket).allocate_page();
+        let page18a = (*bucket).allocate_page();
+        assert_eq!(page16, page16a);
+        assert_eq!(page18, page18a);
+        assert_eq!((*bucket).map, 0);
+        (*bucket).deallocate_page(&*page63);
+        let page63a = (*bucket).allocate_page();
+        assert_eq!(page63, page63a);
         assert_eq!((*bucket).map, 0);
     }
 }
