fe65c662173ef01827ce0eb0edb083dd22a165ac
rschleitzer <ralf.schleitzer@gmx.de>
2019-02-16 07:18:57 -0800

    Heap creates itself and allocates a page.

--- scaly/src/scaly/memory/heap.rs
+++ scaly/src/scaly/memory/heap.rs
@@ -1,9 +1,48 @@
+use scaly::memory::bucket::Bucket;
 use scaly::memory::bucket::BUCKET_PAGES;
+use scaly::memory::page::Page;
 use scaly::memory::pool::Pool;
+use std::ptr::null_mut;
+use std::usize::MAX;
 
 pub struct Heap {
     map: usize,
     pools: [*mut Pool; BUCKET_PAGES],
 }
 
-impl Heap {}
+impl Heap {
+    pub fn create() -> Heap {
+        Heap {
+            map: MAX,
+            pools: [null_mut(); BUCKET_PAGES],
+        }
+    }
+
+    pub fn allocate_page(&mut self) -> *mut Page {
+        // println!("Heap map before allocation: {:X}.", self.map);
+        if self.map == 0 {
+            panic!("Not more than one heap supported currently.");
+        }
+
+        let position = Bucket::find_least_position(self.map);
+        if self.pools[position] == null_mut() {
+            self.pools[position] = Pool::create();
+            println!(
+                "Heap allocated pool at: {:X}.",
+                self.pools[position] as usize
+            );
+        }
+        unsafe { (*self.pools[position]).allocate_page() }
+    }
+}
+
+#[test]
+fn test_heap() {
+    let mut heap = Heap::create();
+    let page = heap.allocate_page();
+    for i in 0..1000 {
+        unsafe {
+            (*page).allocate(i);
+        }
+    }
+}
