4d6bc09bbcbe2dc2a605e3f8d7c89b0e6e3da45c
rschleitzer <ralf.schleitzer@gmx.de>
2019-08-08 07:50:49 -0700

    start work on parser

--- scalyc/src/scalyc/compiler.rs
+++ scalyc/src/scalyc/compiler.rs
@@ -1,27 +1,26 @@
 use scaly::containers::Ref;
-use scaly::io::{Console, ConsoleStream};
-use scaly::io::Stream;
+use scaly::io::Console;
 use scaly::memory::Region;
 use scaly::Page;
 use scaly::String;
 use scalyc::parser::Parser;
 
-pub struct Compiler {
-    parser: Ref<Parser>,
-}
+pub struct Compiler {}
 
 impl Compiler {
     pub fn new(_p: *mut Page) -> Compiler {
-        let parser = Ref::new(_p, Parser::new(_p));
-        Compiler { parser: parser }
+        Compiler {}
     }
     pub fn compile(&self, _pr: &Region) {
         let _r = Region::create(_pr);
         loop {
             let _r_1 = Region::create(&_r);
             Console::write(&_r_1, String::from_string_slice(_r_1.page, "Scaly>"));
-            let parser = Ref::new(_p, Parser::new(_p, Ref::new(_r_1.page, ConsoleStream::));
-            let _statement = (*self.parser).parse_statement(&_r_1, _r_1.page);
+            let parser = Ref::new(
+                _r_1.page,
+                Parser::new(&_r_1, _r_1.page, Console::open_standard_output(_r_1.page)),
+            );
+            let _statement = parser.parse_statement(&_r_1, _r_1.page);
             break;
         }
     }
--- scalyc/src/scalyc/parser.rs
+++ scalyc/src/scalyc/parser.rs
@@ -1,16 +1,22 @@
-use scaly::containers::Ref;
+use scaly::containers::{Array, Ref, String};
+use scaly::io::{Stream};
 use scaly::memory::Region;
 use scaly::Page;
 use scalyc::lexer::Lexer;
 
 pub struct Parser {
     _lexer: Ref<Lexer>,
+    _keywords: Ref<Array<String>>,
 }
 
 impl Parser {
-    pub fn new(_p: *mut Page) -> Parser {
+    pub fn new(_pr: &Region, _rp: *mut Page, stream: *mut Stream) -> Parser {
+        let _r = Region::create(_pr);
+        let keywords_array = Ref::new(_r.page, Array::new());
+        //(*keywords_array).add(_r.page, )
         Parser {
-            _lexer: Ref::new(_p, Lexer::new(_p)),
+            _lexer: Ref::new(_rp, Lexer::new(_rp, stream)),
+            _keywords: keywords_array,
         }
     }
 
