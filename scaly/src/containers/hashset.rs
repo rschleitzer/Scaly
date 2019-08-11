33f4107db94bf3eb17737ea3180b2735685e9717
rschleitzer <ralf.schleitzer@gmx.de>
2019-08-11 03:42:08 -0700

    progress with HashSet

--- scaly/src/containers/hashset.rs
+++ scaly/src/containers/hashset.rs
@@ -1,23 +1,35 @@
-use containers::array::Array;
+use containers::reference::Ref;
+use containers::vector::Vector;
+use memory::page::Page;
 
 pub struct HashSet<T> {
-    _buckets: Array<i32>,
-    _slots: Array<Slot<T>>,
+    _map: Option<Ref<HashMap<T>>>,
 }
 
 impl<T> HashSet<T> {
-    pub fn new() -> HashSet<T> {
+    pub fn from_raw_array(_page: *mut Page, _array: *const T, length: usize) -> HashSet<T> {
         HashSet {
-            _buckets: Array::new(),
-            _slots: Array::new(),
+            _map: Some(Ref::new(_page, HashMap::new(_page, length))),
         }
     }
+}
 
-    pub fn add(_value: T) {}
+struct HashMap<T> {
+    _buckets: Vector<i32>,
+    _slots: Vector<Slot<T>>,
+}
+
+impl<T> HashMap<T> {
+    pub fn new(_page: *mut Page, size: usize) -> HashMap<T> {
+        HashMap {
+            _buckets: Vector::new(_page, size),
+            _slots: Vector::new(_page, size),
+        }
+    }
 }
 
 struct Slot<T> {
     _hash_code: i32,
-    _next: i32,
+    _next: usize,
     _value: T,
 }
--- scaly/src/containers/vector.rs
+++ scaly/src/containers/vector.rs
@@ -1,4 +1,16 @@
+use std::mem::{align_of,size_of};
+use memory::page::Page;
+
 pub struct Vector<T> {
     pub data: *mut T,
     pub length: usize,
+}
+
+impl<T> Vector<T> {
+    pub fn new(_page: *mut Page, length: usize) -> Vector<T> {
+        unsafe { Vector {
+            data: (*_page).allocate_raw(length * size_of::<T>(), align_of::<T>()) as *mut T,
+            length: length,
+        }}
+    }
 }
