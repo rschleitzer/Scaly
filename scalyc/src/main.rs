64f2046df247861ec96c75d2d5ee76a1646d429c
rschleitzer <ralf@scaly.io>
2020-04-12 06:07:01 -0700

    read and print a single card

--- scalyc/src/main.rs
+++ scalyc/src/main.rs
@@ -1,3 +1,10 @@
+use std::io;
+use std::io::Write;
+
 fn main() {
-    println!("Hello, world!");
+    print!("scalyc>");
+    io::stdout().flush().unwrap();
+    let mut card = String::new();
+    io::stdin().read_line(&mut card).expect("failed to read line");
+    println!("The card is {}", card);
 }
