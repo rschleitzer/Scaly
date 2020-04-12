d8324501ae5f105eeb687ebdb4ad0e24018d1b2c
rschleitzer <ralf@scaly.io>
2020-04-12 09:37:03 -0700

    echo loop

--- scalyc/src/main.rs
+++ scalyc/src/main.rs
@@ -2,9 +2,11 @@ use std::io;
 use std::io::Write;
 
 fn main() {
-    print!("scalyc>");
-    io::stdout().flush().unwrap();
-    let mut card = String::new();
-    io::stdin().read_line(&mut card).expect("failed to read line");
-    println!("The card is {}", card);
+    loop {
+        print!("scalyc>");
+        io::stdout().flush().unwrap();
+        let mut card = String::new();
+        io::stdin() .read_line(&mut card).expect("failed to read line");
+        print!("{}", card);
+    }
 }
