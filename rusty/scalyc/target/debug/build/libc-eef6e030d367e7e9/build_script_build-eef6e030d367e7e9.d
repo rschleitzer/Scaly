0d69efc6d376ac5ba78afb7645b080ce07abcbc3
rschleitzer <ralf@scaly.io>
2020-04-12 05:28:35 -0700

    retired scaly and scalyc to rusty directory

---
+++ rusty/scalyc/target/debug/build/libc-eef6e030d367e7e9/build_script_build-eef6e030d367e7e9.d
@@ -0,0 +1,5 @@
+/home/ralf/Scaly/scalyc/target/debug/build/libc-eef6e030d367e7e9/build_script_build-eef6e030d367e7e9: /home/ralf/.cargo/registry/src/github.com-1ecc6299db9ec823/libc-0.2.60/build.rs
+
+/home/ralf/Scaly/scalyc/target/debug/build/libc-eef6e030d367e7e9/build_script_build-eef6e030d367e7e9.d: /home/ralf/.cargo/registry/src/github.com-1ecc6299db9ec823/libc-0.2.60/build.rs
+
+/home/ralf/.cargo/registry/src/github.com-1ecc6299db9ec823/libc-0.2.60/build.rs:
---
+++ rusty/scalyc/target/debug/deps/scalyc-d30fd65b24c74e47.d
@@ -0,0 +1,12 @@
+/home/ralf/Scaly/scalyc/target/debug/deps/scalyc-d30fd65b24c74e47: src/main.rs src/scalyc/mod.rs src/scalyc/options.rs src/scalyc/compiler.rs src/scalyc/errors.rs src/scalyc/parser.rs src/scalyc/lexer.rs src/scalyc/modulebuilder.rs
+
+/home/ralf/Scaly/scalyc/target/debug/deps/scalyc-d30fd65b24c74e47.d: src/main.rs src/scalyc/mod.rs src/scalyc/options.rs src/scalyc/compiler.rs src/scalyc/errors.rs src/scalyc/parser.rs src/scalyc/lexer.rs src/scalyc/modulebuilder.rs
+
+src/main.rs:
+src/scalyc/mod.rs:
+src/scalyc/options.rs:
+src/scalyc/compiler.rs:
+src/scalyc/errors.rs:
+src/scalyc/parser.rs:
+src/scalyc/lexer.rs:
+src/scalyc/modulebuilder.rs:
