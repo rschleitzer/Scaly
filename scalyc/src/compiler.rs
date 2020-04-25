93d4d34516a3e6719c8f460e5f0742cf39b44163
Ralf Schleitzer <ralf@scaly.io>
2020-04-25 13:47:32 +0200

    match on the parsed statment and give feedback

--- scalyc/src/compiler.rs
+++ scalyc/src/compiler.rs
@@ -9,7 +9,16 @@ impl Compiler {
     }
     pub fn evaluate(&mut self, deck: &str) -> String {
         let mut parser = Parser::new(deck);
-        let _statement = parser.parse_statement();
-        String::from(deck)
+        let statement_result = parser.parse_statement();
+        match statement_result {
+            Ok(statement_option) => {
+                match statement_option {
+                    None => return String::from("This is no statement.\n"),
+                    Some(_statement) => return String::from("Parsed successfully.\n"),
+                }
+                
+            }
+            Err(error) => return String::from(format!("Parser error at {}, {}\n", error.line, error.column)),
+        }
     }
 }
