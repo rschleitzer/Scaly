6a50cebf71020616183585d0253f04d7862beef0
rschleitzer <ralf@scaly.io>
2022-04-01 09:44:11 +0200

    Ported Page from Rust back to C++.

---
+++ runtime/memory/Page.cpp
@@ -0,0 +1,264 @@
+#ifndef __Scaly_Page__
+#define __Scaly_Page__
+
+#include <stdlib.h>
+
+using std::size_t;
+
+namespace scaly::memory {
+
+const int PAGE_SIZE = 0x1000;
+
+struct Page {
+    Page* current_page;
+    int next_object_offset;
+    int exclusive_pages;
+    void reset() {
+        // *(this->get_extension_page_location()) = nullptr;
+        this->next_object_offset = sizeof(Page);
+        this->exclusive_pages = 0;
+        this->current_page = this;
+    }
+
+    void clear() {
+        // this->deallocate_extensions();
+        this->reset();
+    }
+
+    bool is_oversized() {
+        return this->current_page == nullptr;
+    }
+
+    Page** get_extension_page_location() {
+        // Convert self to page location
+        auto self_location = (char*)this;
+
+        // Advance one page size so we are past the end of our page
+        auto location_behind_page = (Page**)(self_location + PAGE_SIZE);
+
+        // Go back one pointer size
+        return location_behind_page - 1;
+    }
+
+    size_t get_next_location() {
+        return ((size_t)this) + this->next_object_offset;
+    }
+
+    void set_next_location(size_t location) {
+        auto self_location = (size_t)this;
+        this->next_object_offset = (int)(location - self_location);
+    }
+
+    Page** get_next_exclusive_page_location() {
+        return this->get_extension_page_location() - this->exclusive_pages + 1;
+    }
+
+    size_t get_capacity(size_t align) {
+        auto location = this->get_next_location();
+        auto aligned_location = (location + align - 1) & !(align - 1);
+        return (size_t)(this->get_next_exclusive_page_location()) - aligned_location;
+    }
+
+    void* allocate_raw(size_t size, size_t align) {
+        if (this != this->current_page) {
+            // We're already known to be full, so we delegate to the current page
+            auto new_object = (this->current_page)->allocate_raw(size, align);
+
+            // Possibly our current page was also full so we propagate back the new current page
+            auto allocating_page = Page::get((size_t)new_object);
+            if (allocating_page != this->current_page && (!(*allocating_page).is_oversized())) {
+                this->current_page = allocating_page;
+            }
+            return new_object;
+        }
+
+        // Try to allocate from ourselves
+        auto location = this->get_next_location();
+        auto aligned_location = (location + align - 1) & ~(align - 1);
+        auto next_location = aligned_location + size;
+        if (next_location <= (size_t)this->get_next_exclusive_page_location()) {
+            this->set_next_location(next_location);
+            return (void*)location;
+        }
+
+        // So the space did not fit.
+
+        // Calculate gross size to decide whether we're oversized
+        if (sizeof(Page) + size + sizeof(Page**) > PAGE_SIZE) {
+            if (this->get_next_location() >= (size_t)(this->get_next_exclusive_page_location())) {
+                // Allocate an extension page and try again with it
+                return (this->allocate_extension_page())->allocate_raw(size, align);
+            }
+
+            auto gross_size = size + sizeof(Page);
+            // We allocate oversized objects directly.
+            Page* page;
+            posix_memalign((void**)&page, PAGE_SIZE, size + sizeof(Page));
+
+            // Oversized pages have no current_page
+            page->current_page = nullptr;
+
+            // Set the size since we will need it when deallocating
+            page->next_object_offset = (int)(gross_size % 0x100000000);
+            page->exclusive_pages = (int)(gross_size / 0x100000000);
+
+            *(this->get_next_exclusive_page_location()) = page;
+            this->exclusive_pages += 1;
+            return page + 1;
+        }
+
+        // So we're not oversized. Create extension page and let it allocate.
+        return (this->allocate_extension_page())->allocate_raw(size, align);
+    }
+
+    Page* allocate_extension_page() {
+        auto page = this->allocate_page();
+        // println!("Allocated extension page: {:X}", page as usize);
+        *(this->get_extension_page_location()) = page;
+        this->current_page = page;
+        return page;
+    }
+
+    Page* allocate_page() {
+        // let bucket = Bucket::get(self as *const Page as usize);
+        // // println!("Bucket: {:X}", bucket as usize);
+        // let page = (*bucket).allocate_page();
+        // page
+        Page* page;
+        posix_memalign((void**)&page, PAGE_SIZE, PAGE_SIZE);
+        page->reset();
+        return page;
+    }
+
+    Page* allocate_exclusive_page() {
+        if (this != this->current_page) {
+            // We're already known to be full, so we delegate to the current page
+            return this->current_page->allocate_exclusive_page();
+        }
+
+        // Check first whether we need an ordinary extension
+        if (((size_t)this->get_next_location()) >= (size_t)(this->get_next_exclusive_page_location()))
+        {
+            // Allocate an extension page with default size
+            return this->allocate_extension_page()->allocate_exclusive_page();
+        }
+
+        auto page = this->allocate_page();
+        *(this->get_next_exclusive_page_location()) = page;
+        this->exclusive_pages += 1;
+        return page;
+    }
+
+    bool extend(size_t top, size_t size) {
+        if (top != this->get_next_location()) {
+            return false;
+        }
+
+        auto new_top = top + size;
+        if (new_top > (size_t)this->get_next_exclusive_page_location()) {
+            return false;
+        }
+        this->set_next_location(new_top);
+
+        return true;
+    }
+
+    void deallocate_extensions() {
+        auto page = this;
+        while (page != nullptr) {
+            auto extension_pointer = page->get_extension_page_location();
+            for (int i = 1; i <= page->exclusive_pages; i++) {
+                auto exclusive_page = *(extension_pointer - i);
+                if (!exclusive_page->is_oversized()) {
+                    exclusive_page->deallocate_extensions();
+                }
+                exclusive_page->forget();
+            }
+
+            if (page != this) {
+                page->forget();
+            }
+
+            page = *extension_pointer;
+        }
+    }
+
+    void forget() {
+        //println!("Page: dealloc {:X}", self as *const Page as usize);
+        if (this->current_page == nullptr) {
+            free (this);
+            // dealloc(
+            //     self as *const Page as *mut u8,
+            //     Layout::from_size_align_unchecked(
+            //         self.next_object_offset as usize
+            //             + self.exclusive_pages as usize * 0x100000000,
+            //         PAGE_SIZE,
+            //     ),
+            // );
+        } else {
+            free(this);
+            // let bucket = Bucket::get(self as *const Page as usize);
+            // //println!("Bucket: {:X}", bucket as usize);
+            // (*bucket).deallocate_page(self);
+        }
+    }
+
+    bool reclaim_array(Page* address) {
+        auto page = this;
+        while (page != nullptr) {
+            if (page->deallocate_exclusive_page(address)) {
+                return true;
+            }
+            page = *(page->get_extension_page_location());
+        }
+
+        // If we arrive here, we have a memory leak.
+        return false;
+    }
+
+    static Page* get(size_t address) {
+        auto address_raw = (void*)address;
+        auto mask = ~(size_t)(PAGE_SIZE - 1);
+        auto mask_raw = (void*)mask;
+        auto page_raw = (void*)(address & mask);
+        auto page = (Page*)page_raw;
+        return page;
+    }
+
+    // pub fn own<T>(object: Ref<T>) -> *mut Page {
+    //     Page::get(object.data as usize)
+    // }
+
+    bool deallocate_exclusive_page(Page* page) {
+        // Find the extension Page pointer
+        auto extension_pointer = this->get_extension_page_location() - 1;
+        auto next_extension_page_location = this->get_next_exclusive_page_location();
+        while (extension_pointer > next_extension_page_location) {
+            if (*extension_pointer == page) {
+                break;
+            }
+            extension_pointer = extension_pointer - 1;
+        }
+
+        // Report if we could not find it
+        if (extension_pointer == next_extension_page_location) {
+            return false;
+        }
+
+        // Shift the remaining array one position up
+        while (extension_pointer > next_extension_page_location) {
+            auto pp_page = extension_pointer;
+            *pp_page = *(pp_page - 1);
+            extension_pointer = extension_pointer - 1;
+        }
+
+        // Make room for one more extension
+        this->exclusive_pages -= 1;
+            page->forget();
+
+        return true;
+    }
+};
+
+}
+#endif // __Scaly_Page__
---
+++ runtime/memory/main.cpp
@@ -0,0 +1,140 @@
+#include "Page.cpp"
+using namespace scaly::memory;
+
+int main(int argc, char** argv)
+{
+    Page* page;
+    posix_memalign((void**)&page, PAGE_SIZE, PAGE_SIZE);
+    page->reset();
+    // let mut heap = Heap::create();
+    // let root_stack_bucket = StackBucket::create(&mut heap);
+    // let r = Region::create_from_page(&*Page::get(root_stack_bucket as usize));
+
+    // assert_eq!(r.page.next_object_offset, size_of::<Page>() as i32);
+    if (page->next_object_offset != sizeof(Page))
+        return 1;
+
+    // assert_eq!(r.page.exclusive_pages, 0);
+    if (page->exclusive_pages != 0)
+        return 1;
+
+    // assert_eq!(r.page.is_oversized(), false);
+    if (page->is_oversized())
+        return 3;
+
+    // assert_eq!(r.page as *mut Page, r.page.current_page);
+    if (page != page->current_page)
+        return 4;
+
+    auto extension_page_location = page->get_extension_page_location();
+    if ((extension_page_location != (Page**)((size_t)page + PAGE_SIZE - sizeof(Page**))))
+        return 5;
+
+    auto location = page->get_next_location();
+    if (location != (size_t)((char*)page + sizeof(Page)))
+        return 6;
+
+    auto answer = (int*)page->allocate_raw(4, 4);
+    location += 4;
+    if (page->get_next_location() != location)
+        return 7;
+    *answer = 42;
+
+    auto another_loc = page->allocate_raw(1, 2);
+    auto another = (char*)another_loc;
+    location += 1;
+    if (page->get_next_location() != location)
+        return 8;
+
+    *another = 'A';
+
+    auto eau_loc = page->allocate_raw(4, 4);
+    auto eau = (int*)eau_loc;
+    location += 7;
+    if (page->get_next_location() != location)
+        return 9;
+    *eau = 4711;
+
+    if (Page::get((size_t)eau) != page)
+        return 10;
+
+    // Allocate an oversized object which should cause allocating an exclusive page
+    auto array = (size_t*)page->allocate_raw(PAGE_SIZE, 8);
+    for (int i = 0; i <= PAGE_SIZE / sizeof(size_t) - 1; i++) {
+        array[i] = i;
+    }
+    
+    // After allocating the oversized object, the location shall be the same
+    if (page->get_next_location() != location)
+        return 11;
+
+    // The current page should be still us
+    if (page != page->current_page)
+        return 12;
+
+    // Overflow the page
+    for (int i = 0; i <= PAGE_SIZE; i++) {
+        page->allocate_raw(1, 1);
+    }
+
+    // Now the current page shall have switched to a new one
+    if (page == page->current_page)
+        return 13;
+
+    // Since we have one extension page now, our extension page shall point to our current page.
+    if (*(page->get_extension_page_location()) != page->current_page)
+        return 14;
+
+    // Our new current page shall not have exclusive pages yet.
+    if (page->current_page->exclusive_pages != 0)
+        return 15;
+
+    // The current page of our new current page shall point to itself.
+    if (page->current_page->current_page != page->current_page)
+        return 16;
+
+    if (page->exclusive_pages != 1)
+        return 17;
+
+    // The data shall be still intact.
+    if (*answer != 42)
+        return 18;
+
+    if (*another != 'A')
+        return 19;
+
+    if (*eau  != 4711)
+        return 20;
+
+    // Now we look at our exclusive page which is one pointer above the location of our next exclusive page.
+    auto exclusive_page = *(page->get_next_exclusive_page_location() + 1);
+    
+    // The exclusive page shall not have a current page.
+    if (exclusive_page->current_page != nullptr)
+        return 21;
+
+    // The upper part of the allocated size was stored in exclusive_pages which shall be zero since we did a rather small allocation.
+    if (exclusive_page->exclusive_pages != 0)
+        return 22;
+
+    // The lower part of the allocated size was stored in next_object_offset.
+    if (exclusive_page->next_object_offset != sizeof(Page) + PAGE_SIZE)
+        return 23;
+
+    // The page of our array pointer shall be our exclusive page.
+    if (Page::get((size_t)array) != exclusive_page)
+        return 24;
+
+    // Free the array storage.
+    if (!page->reclaim_array(Page::get((size_t)array)))
+        return 25;
+
+    // Our page shall have no more exclusive pages now.
+    if (page->exclusive_pages != 0)
+        return 26;
+
+    page->clear();
+    page->forget();
+
+    return 0;
+}
