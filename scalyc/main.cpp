1970cac304f33cfa7e5b2c15bb56bfc9cdde0bab
rschleitzer <ralf@scaly.io>
2022-08-30 08:20:55 +0200

    Bare bones of scalyc

---
+++ scalyc/main.cpp
@@ -0,0 +1,22 @@
+#include "../scaly/containers/Containers.cpp"
+
+using namespace scaly::containers;
+
+int scalyc(Vector<String>& arguments) {
+    return 0;
+}
+
+int main(int argc, char** argv) {
+    Region _r;
+    List<String> list;
+    int i = 0;
+    while (i < argc) {
+        char* raw_argument = argv[i];
+        String argument(_r.get_page(), raw_argument);
+        list.add(_r.get_page(), argument);
+        i++;
+    }
+    Vector<String> arguments(_r.get_page(), list);
+
+    return scalyc(arguments);
+}
