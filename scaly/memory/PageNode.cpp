5d95a620dbff63bf2927d34bd0f85c3053993322
rschleitzer <ralf@scaly.io>
2024-09-19 22:50:45 +0200

    Some reafactroring

--- scaly/memory/PageNode.cpp
+++ scaly/memory/PageNode.cpp
@@ -1,3 +1,15 @@
+#include <stdlib.h>
+#include <new>
+#include <type_traits>
+#include <limits>
+
+using std::size_t;
+
+const int PAGE_SIZE = 0x1000;
+
+#include "Object.h"
+#include "PageNode.h"
+
 scaly::memory::PageNode::PageNode(Page* thePage, PageNode* nextNode)
     :   page(thePage),
         next(nextNode) {};
