ad570eb67edff5377b3b23e3046fabcdf5d00b5d
rschleitzer <ralf@scaly.io>
2024-10-04 14:19:24 +0200

    Transpiler progress

--- compiler/Transpiler.cpp
+++ compiler/Transpiler.cpp
@@ -18,9 +18,7 @@ struct Transpiler : Object {
             if (_result != nullptr)
                 return new(alignof(TranspilerError), _ep) TranspilerError(*_result);
         }
-        StringBuilder& main_header_builder = *new (alignof(StringBuilder), _r.get_page()) StringBuilder("../");
-        main_header_builder.append(program.module.name);
-        auto _result = build_module(_ep, path, program.module, main_header_builder.to_string(_r.get_page()), String(), String());
+        auto _result = build_module(_ep, path, program.module, program.module.name, String(), String());
         if (_result != nullptr)
             return new(alignof(TranspilerError), _ep) TranspilerError(*_result);
 
@@ -37,10 +35,11 @@ struct Transpiler : Object {
     TranspilerError* build_module(Page* _ep, String path, Module& module, String main_header, String namespace_open, String namespace_close) {
         Region _r;
 
-        auto _path_result = extend_and_create_path(_r.get_page(), _ep, path, module.name);
-        if (_path_result._tag == Result<String, FileError>::Error)
-            return new(alignof(TranspilerError), _ep) TranspilerError(_path_result._Error);
-        path = _path_result._Ok;
+        auto _path_result = create_directory(_ep, path);
+        if (_path_result != nullptr)
+            return new(alignof(TranspilerError), _ep) TranspilerError(*_path_result);
+
+        path = Path::join(_r.get_page(), path, module.name);
 
         StringBuilder& header_builder = *new (alignof(StringBuilder), _r.get_page()) StringBuilder();
         StringBuilder& cpp_builder = *new (alignof(StringBuilder), _r.get_page()) StringBuilder();
@@ -50,12 +49,32 @@ struct Transpiler : Object {
         cpp_builder.append(".h\"\n");
         cpp_builder.append(namespace_open);
 
+        header_builder.append("#ifndef _");
+        header_builder.append(module.name);
+        header_builder.append("_h\n");
+        header_builder.append("#define _");
+        header_builder.append(module.name);
+        header_builder.append("_h\n");
+        if (module.name.equals("scaly")) {
+            header_builder.append("#include \"forwards.h\"\n");
+        }
+        else {
+            header_builder.append("#include \"");
+            header_builder.append(main_header);
+            header_builder.append(".h\"\n");
+        }
+        if (!module.name.equals("scaly") && !module.name.equals("memory"))
+            header_builder.append("\nusing namespace scaly::memory;\n");
+        header_builder.append(namespace_open);
+
         auto _symbols_result = build_symbols(_ep, path, module.file, module.name, header_builder, cpp_builder, main_header, namespace_open, namespace_close, module.symbols);
         if (_symbols_result)
             return _symbols_result;
 
         cpp_builder.append('\n');
         cpp_builder.append(namespace_close);
+        header_builder.append(namespace_close);
+        header_builder.append("\n#endif");
 
         StringBuilder& header_name_builder = *new(alignof(StringBuilder), _r.get_page()) StringBuilder(path);
         header_name_builder.append(".h");
@@ -74,15 +93,7 @@ struct Transpiler : Object {
         return nullptr;
     }
 
-    Result<String, FileError> extend_and_create_path(Page* _rp, Page* _ep, String path, String name) {
-        path = Path::join(_rp, path, name);
-        auto _create_directory_result = create_directory(_rp, _ep, path);
-        if (_create_directory_result != nullptr)
-            return Result<String, FileError> { ._tag = Result<String, FileError>::Error, ._Error = *_create_directory_result };
-        return Result<String, FileError> { ._tag = Result<String, FileError>::Ok, ._Ok = path };
-    }
-
-    FileError* create_directory(Page* _rp, Page* _ep, String path) {
+    FileError* create_directory(Page* _ep, String path) {
         auto _exists_result = Directory::exists(_ep, path);
         if (_exists_result._tag == scaly::containers::Result<bool, FileError>::Error)
             return new(alignof(FileError), _ep) FileError(_exists_result._Error);
@@ -95,9 +106,7 @@ struct Transpiler : Object {
         return nullptr;
     }
 
-    TranspilerError* build_concept(Page* _ep, String path, String source, StringBuilder& header_builder, StringBuilder& cpp_builder, String main_header, String namespace_open, String namespace_close, Concept& concept) {
-        Region _r;
- 
+    TranspilerError* build_concept(Page* _ep, String path, String source, StringBuilder& header_builder, StringBuilder& cpp_builder, String main_header, String namespace_open, String namespace_close, Concept& concept) { 
         switch (concept.definition._tag) {
             case Definition::Namespace: {
                 auto _namespace = concept.definition._Namespace;
@@ -122,7 +131,18 @@ struct Transpiler : Object {
     }
 
     TranspilerError* build_namespace(Page* _ep, String path, String source, String name, StringBuilder& header_builder, StringBuilder& cpp_builder, String main_header, String namespace_open, String namespace_close, const Namespace& namespace_) {
-        return build_symbols(_ep, path, source, name, header_builder, cpp_builder, main_header, namespace_open, namespace_close, namespace_.symbols);
+        Region _r;
+        StringBuilder& namespace_open_builder = *new(alignof(StringBuilder), _r.get_page()) StringBuilder(namespace_open);
+        namespace_open_builder.append("namespace ");
+        namespace_open_builder.append(name);
+        namespace_open_builder.append(" {\n");
+        namespace_open = namespace_open_builder.to_string(_r.get_page());
+        StringBuilder& namespace_close_builder = *new(alignof(StringBuilder), _r.get_page()) StringBuilder(namespace_close);
+        namespace_close_builder.append("\n}");
+        namespace_close = namespace_close_builder.to_string(_r.get_page());
+        StringBuilder& main_header_builder = *new (alignof(StringBuilder), _r.get_page()) StringBuilder("../");
+        main_header_builder.append(main_header);
+        return build_symbols(_ep, path, source, name, header_builder, cpp_builder, main_header_builder.to_string(_r.get_page()), namespace_open, namespace_close, namespace_.symbols);
     }
 
     TranspilerError* build_symbols(Page* _ep, String path, String source, String name, StringBuilder& header_builder, StringBuilder& cpp_builder, String main_header, String namespace_open, String namespace_close, HashMap<String, Nameable> symbols) {
@@ -145,10 +165,8 @@ struct Transpiler : Object {
                         header_builder.append('/');
                         header_builder.append(String(_r.get_page(), _module.name));
                         header_builder.append(".h\"\n");
-                        StringBuilder& main_header_builder = *new (alignof(StringBuilder), _r.get_page()) StringBuilder();
-                        main_header_builder.append(main_header);
                         StringBuilder& namespace_close_builder = *new (alignof(StringBuilder), _r.get_page()) StringBuilder(namespace_close);
-                        auto _result = build_module(_ep, path, _module, main_header_builder.to_string(_r.get_page()), namespace_open, namespace_close);
+                        auto _result = build_module(_ep, path, _module, main_header, namespace_open, namespace_close);
                         if (_result != nullptr)
                             return new(alignof(TranspilerError), _ep) TranspilerError(*_result);
                     }
@@ -172,7 +190,6 @@ struct Transpiler : Object {
 
     TranspilerError* build_structure(Page* _ep, StringBuilder& header_builder, StringBuilder& cpp_builder, String name, Structure& structure, Vector<GenericParameter> parameters) {
         Region _r;
-        header_builder.append("\nusing namespace scaly::memory;\n");
         full_struct_name(header_builder, name, parameters);
         if (!name.equals(String(_r.get_page(), "Object")))
             header_builder.append(" : Object");
@@ -436,13 +453,9 @@ namespace scaly { namespace memory { struct Page; } }\n\
 typedef __SIZE_TYPE__ size_t;\n\
 typedef const char const_char;\n\
 #include \"scaly/memory/Object.h\"\n");
-        builder.append("namespace ");
-        builder.append(program.module.name);
-        builder.append(" {\n");
         auto _result = forward_includes_for_symbols(_ep, builder,  program.module.symbols);
         if (_result != nullptr)
             return new(alignof(TranspilerError), _ep) TranspilerError(*_result);
-        builder.append("}\n");
 
         if (builder.get_length() > 0) {
             auto main_file_name = Path::join(_r.get_page(), path, String(_r.get_page(), "forwards.h"));
@@ -524,7 +537,7 @@ typedef const char const_char;\n\
     TranspilerError* vscode_files(Page* _ep, String path, Program& program) {
         Region _r;
         auto vscode_dir = Path::join(_r.get_page(), path, String(_r.get_page(), ".vscode"));
-        auto _create_directory_result = create_directory(_r.get_page(), _ep, vscode_dir);
+        auto _create_directory_result = create_directory(_ep, vscode_dir);
         if (_create_directory_result != nullptr)
             return new(alignof(TranspilerError), _ep) TranspilerError(*_create_directory_result);
         {
@@ -677,7 +690,7 @@ typedef const char const_char;\n\
         Region _r;
         StringBuilder& main_include_file_builder = *new(alignof(StringBuilder), _r.get_page()) StringBuilder(Path::join(_r.get_page(), path, String(_r.get_page(), "main")));
         main_include_file_builder.append(".h");
-        StringBuilder& main_include_builder = *new(alignof(StringBuilder), _r.get_page()) StringBuilder("#include \"forwards.h\"\n");
+        StringBuilder& main_include_builder = *new(alignof(StringBuilder), _r.get_page()) StringBuilder();
         main_include_builder.append("#include \"");
         main_include_builder.append(name);
         main_include_builder.append(".h\"\n");
