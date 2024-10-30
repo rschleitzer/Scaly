709e46911fb28578b9f33e2278829ebecd6a5604
rschleitzer <ralf@scaly.io>
2024-10-30 10:37:45 +0100

    Moved Result stuff to scaly.h

--- output/scaly.h
+++ output/scaly.h
@@ -9,6 +9,21 @@
 #include <math.h>
 #include <libgen.h>
 #include "forwards.h"
+
+struct Void {};
+enum Success { Ok, Error };
+
+template<class OK, class ERROR>
+struct Result {
+    Result(OK _Ok) : _tag(Ok), _Ok(_Ok) {}
+    Result(ERROR _ERROR) : _tag(Error), _Error(_ERROR) {}
+    Success _tag;
+    union {
+        OK _Ok;
+        ERROR _Error;
+    };
+};
+
 namespace scaly {
 #include "scaly/Result.h"
 #include "scaly/memory.h"
