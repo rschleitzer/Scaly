dbee28078a9526f20cc8ef82f8a7094fce46f389
rschleitzer <ralf@scaly.io>
2024-10-31 08:18:47 +0100

    Pre-implement new simplified try transpilation avoiding the else branch by speculation

--- output/scaly/io.cpp
+++ output/scaly/io.cpp
@@ -3,43 +3,19 @@ namespace scaly {
 
 void io::test_file() {
     auto r = Region();
-    const auto _file_not_found_result = File::read_to_string(r.get_page(), r.get_page(), String(r.get_page(), "foo"));
-    if (_file_not_found_result._tag == Success::Error) {
-        const auto _file_not_found_Error = _file_not_found_result._Error;
-        switch (_file_not_found_Error._tag) {
-            case FileError::NoSuchFileOrDirectory: {
-                const auto error = _file_not_found_Error._NoSuchFileOrDirectory;
-                {
-                    const auto _text_result = File::read_to_string(r.get_page(), r.get_page(), String(r.get_page(), "bar"));
-                    if (_text_result._tag == Success::Error) {
-                        const auto _text_Error = _text_result._Error;
-                        switch (_text_Error._tag) {
-                            default: {
-                                exit(-2);
-                                break;
-                            }
-                        }
-                    }
-                    else
-                    {
-                        auto text = _text_result._Ok;
-                        if (text.equals(String(r.get_page(), "baz")) == false) 
-                            exit(-3);
-                    }
-                }
-                break;
-            }
+    const auto _text_result = File::read_to_string(r.get_page(), r.get_page(), String(r.get_page(), "bar"));
+    auto text = _text_result._Ok;
+    if (_text_result._tag == Success::Error) {
+        const auto _text_Error = _text_result._Error;
+        switch (_text_Error._tag) {
             default: {
-                exit(-4);
+                exit(-2);
                 break;
             }
         }
     }
-    else
-    {
-        auto file_not_found = _file_not_found_result._Ok;
-        exit(-1);
-    }
+    if (text.equals(String(r.get_page(), "baz")) == false) 
+        exit(-3);
 }
 
 void io::test() {
