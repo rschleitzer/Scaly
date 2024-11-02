841924de596835101d98eae6b6f37782933e42df
rschleitzer <ralf@scaly.io>
2024-11-02 08:16:13 +0100

    progress with Lexer tests

--- output/scaly/compiler.cpp
+++ output/scaly/compiler.cpp
@@ -125,6 +125,335 @@ abc_AZ0815_7 42 \n : 0 012 0.34 0.56E12 0.78e13 0xaB 0xCdEf02 0B0 0B1\n\
                     exit(9);
             }
         };
+        {
+            auto _result = lexer.token;
+            switch (_result._tag)
+            {
+                case Token::Colon:
+                {
+                    auto c = _result._Colon;
+                    lexer.advance();
+                    break;
+                }
+                default:
+                    exit(10);
+            }
+        };
+        {
+            auto _result = lexer.token;
+            switch (_result._tag)
+            {
+                case Token::Literal:
+                {
+                    auto literal = _result._Literal;
+                    {
+                        {
+                            auto _result = literal;
+                            switch (_result._tag)
+                            {
+                                case LiteralToken::Integer:
+                                {
+                                    auto integer = _result._Integer;
+                                    {
+                                        if (String(r.get_page(), "0").equals(integer.value) == false) 
+                                            exit(11);
+                                        lexer.advance();
+                                    };
+                                    break;
+                                }
+                                default:
+                                    exit(12);
+                            }
+                        };
+                    };
+                    break;
+                }
+                default:
+                    exit(13);
+            }
+        };
+        {
+            auto _result = lexer.token;
+            switch (_result._tag)
+            {
+                case Token::Literal:
+                {
+                    auto literal = _result._Literal;
+                    {
+                        {
+                            auto _result = literal;
+                            switch (_result._tag)
+                            {
+                                case LiteralToken::Integer:
+                                {
+                                    auto integer = _result._Integer;
+                                    {
+                                        if (String(r.get_page(), "012").equals(integer.value) == false) 
+                                            exit(14);
+                                        lexer.advance();
+                                    };
+                                    break;
+                                }
+                                default:
+                                    exit(15);
+                            }
+                        };
+                    };
+                    break;
+                }
+                default:
+                    exit(16);
+            }
+        };
+        {
+            auto _result = lexer.token;
+            switch (_result._tag)
+            {
+                case Token::Literal:
+                {
+                    auto literal = _result._Literal;
+                    {
+                        {
+                            auto _result = literal;
+                            switch (_result._tag)
+                            {
+                                case LiteralToken::FloatingPoint:
+                                {
+                                    auto fp = _result._FloatingPoint;
+                                    {
+                                        if (String(r.get_page(), "0.34").equals(fp.value) == false) 
+                                            exit(17);
+                                        lexer.advance();
+                                    };
+                                    break;
+                                }
+                                default:
+                                    exit(18);
+                            }
+                        };
+                    };
+                    break;
+                }
+                default:
+                    exit(19);
+            }
+        };
+        {
+            auto _result = lexer.token;
+            switch (_result._tag)
+            {
+                case Token::Literal:
+                {
+                    auto literal = _result._Literal;
+                    {
+                        {
+                            auto _result = literal;
+                            switch (_result._tag)
+                            {
+                                case LiteralToken::FloatingPoint:
+                                {
+                                    auto fp = _result._FloatingPoint;
+                                    {
+                                        if (String(r.get_page(), "0.56E12").equals(fp.value) == false) 
+                                            exit(20);
+                                        lexer.advance();
+                                    };
+                                    break;
+                                }
+                                default:
+                                    exit(21);
+                            }
+                        };
+                    };
+                    break;
+                }
+                default:
+                    exit(22);
+            }
+        };
+        {
+            auto _result = lexer.token;
+            switch (_result._tag)
+            {
+                case Token::Literal:
+                {
+                    auto literal = _result._Literal;
+                    {
+                        {
+                            auto _result = literal;
+                            switch (_result._tag)
+                            {
+                                case LiteralToken::FloatingPoint:
+                                {
+                                    auto fp = _result._FloatingPoint;
+                                    {
+                                        if (String(r.get_page(), "0.78e13").equals(fp.value) == false) 
+                                            exit(23);
+                                        lexer.advance();
+                                    };
+                                    break;
+                                }
+                                default:
+                                    exit(24);
+                            }
+                        };
+                    };
+                    break;
+                }
+                default:
+                    exit(25);
+            }
+        };
+        {
+            auto _result = lexer.token;
+            switch (_result._tag)
+            {
+                case Token::Literal:
+                {
+                    auto literal = _result._Literal;
+                    {
+                        {
+                            auto _result = literal;
+                            switch (_result._tag)
+                            {
+                                case LiteralToken::Hex:
+                                {
+                                    auto hex = _result._Hex;
+                                    {
+                                        if (String(r.get_page(), "0xaB").equals(hex.value) == false) 
+                                            exit(26);
+                                        lexer.advance();
+                                    };
+                                    break;
+                                }
+                                default:
+                                    exit(27);
+                            }
+                        };
+                    };
+                    break;
+                }
+                default:
+                    exit(28);
+            }
+        };
+        {
+            auto _result = lexer.token;
+            switch (_result._tag)
+            {
+                case Token::Literal:
+                {
+                    auto literal = _result._Literal;
+                    {
+                        {
+                            auto _result = literal;
+                            switch (_result._tag)
+                            {
+                                case LiteralToken::Hex:
+                                {
+                                    auto hex = _result._Hex;
+                                    {
+                                        if (String(r.get_page(), "0xCdEf02").equals(hex.value) == false) 
+                                            exit(29);
+                                        lexer.advance();
+                                    };
+                                    break;
+                                }
+                                default:
+                                    exit(30);
+                            }
+                        };
+                    };
+                    break;
+                }
+                default:
+                    exit(31);
+            }
+        };
+        {
+            auto _result = lexer.token;
+            switch (_result._tag)
+            {
+                case Token::Literal:
+                {
+                    auto literal = _result._Literal;
+                    {
+                        {
+                            auto _result = literal;
+                            switch (_result._tag)
+                            {
+                                case LiteralToken::Boolean:
+                                {
+                                    auto b = _result._Boolean;
+                                    {
+                                        if (b.value) 
+                                            exit(32);
+                                        lexer.advance();
+                                    };
+                                    break;
+                                }
+                                default:
+                                    exit(33);
+                            }
+                        };
+                    };
+                    break;
+                }
+                default:
+                    exit(34);
+            }
+        };
+        {
+            auto _result = lexer.token;
+            switch (_result._tag)
+            {
+                case Token::Literal:
+                {
+                    auto literal = _result._Literal;
+                    {
+                        {
+                            auto _result = literal;
+                            switch (_result._tag)
+                            {
+                                case LiteralToken::Boolean:
+                                {
+                                    auto b = _result._Boolean;
+                                    {
+                                        if (b.value == false) 
+                                            exit(35);
+                                        lexer.advance();
+                                    };
+                                    break;
+                                }
+                                default:
+                                    exit(36);
+                            }
+                        };
+                    };
+                    break;
+                }
+                default:
+                    exit(37);
+            }
+        };
+        {
+            auto _result = lexer.token;
+            switch (_result._tag)
+            {
+                case Token::Attribute:
+                {
+                    auto a = _result._Attribute;
+                    {
+                        if (String(r.get_page(), "ttribute").equals(a.name) == false) 
+                            exit(38);
+                        lexer.advance();
+                    };
+                    break;
+                }
+                default:
+                    exit(39);
+            }
+        };
     };
 }
 
