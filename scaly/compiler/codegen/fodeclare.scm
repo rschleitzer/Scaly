0f9c94ee241c2f8251b984478f03a7b806785f1e
rschleitzer <ralf@scaly.io>
2024-11-03 08:33:50 +0100

    copied the parser codegen

---
+++ scaly/compiler/codegen/fodeclare.scm
@@ -0,0 +1,27 @@
+(declare-flow-object-class element
+  "UNREGISTERED::James Clark//Flow Object Class::element")
+
+(declare-flow-object-class empty-element
+  "UNREGISTERED::James Clark//Flow Object Class::empty-element")
+
+(declare-flow-object-class document-type
+  "UNREGISTERED::James Clark//Flow Object Class::document-type")
+
+(declare-flow-object-class processing-instruction
+  "UNREGISTERED::James Clark//Flow Object Class::processing-instruction")
+
+(declare-flow-object-class entity
+  "UNREGISTERED::James Clark//Flow Object Class::entity")
+
+(declare-flow-object-class entity-ref
+  "UNREGISTERED::James Clark//Flow Object Class::entity-ref")
+
+(declare-flow-object-class formatting-instruction
+  "UNREGISTERED::James Clark//Flow Object Class::formatting-instruction")
+
+(declare-characteristic preserve-sdata?
+  "UNREGISTERED::James Clark//Characteristic::preserve-sdata?"
+  #f)
+
+(define debug
+    (external-procedure "UNREGISTERED::James Clark//Procedure::debug"))
---
+++ scaly/compiler/codegen/rules.scm
@@ -0,0 +1,13 @@
+(element grammar
+    (file ($ "Parser.cpp")
+        ($
+"namespace scaly {
+namespace compiler {
+"
+            (syntax)
+            (parser)
+"}
+}
+"        )
+    )
+)
