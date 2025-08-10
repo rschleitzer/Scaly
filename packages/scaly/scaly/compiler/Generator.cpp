cad34964db30b29861987eb58060e4e92ac2c584
rschleitzer <ralf@scaly.io>
2025-08-10 18:00:38 +0200

    Added some stub debug info.

--- packages/scaly/scaly/compiler/Generator.cpp
+++ packages/scaly/scaly/compiler/Generator.cpp
@@ -23,7 +23,10 @@
 #include "llvm/IR/Type.h"
 #include "llvm/IR/Verifier.h"
 #include "llvm/ExecutionEngine/Orc/LLJIT.h"
+#include "llvm/ExecutionEngine/SectionMemoryManager.h"
+#include "llvm/ExecutionEngine/Orc/DebugObjectManagerPlugin.h"
 #include "llvm/Support/TargetSelect.h"
+#include <llvm/IR/DIBuilder.h>
 
 struct Void {};
 enum Success { Ok, Error };
@@ -50,6 +53,7 @@ namespace scaly {
 #endif
 
 using namespace llvm;
+using namespace llvm::orc;
 
 namespace scaly {
 namespace compiler {
@@ -99,7 +103,7 @@ namespace generator {
         }
     }
 
-    llvm::Function* generate_function(llvm::Module& module, Plan::Function& plan_function) {
+    llvm::Function* generate_function(llvm::Module& module, DIFile* di_file, DIBuilder& di_builder, Plan::Function& plan_function) {
         auto r = Region();
         LLVMContext& context = module.getContext();
         llvm::Type* return_type = get_Type(context, plan_function.output);
@@ -113,7 +117,8 @@ namespace generator {
         }
 
         FunctionType* llvm_function_type = FunctionType::get(return_type, llvm_argument_types, false);
-        llvm::Function* llvm_function = llvm::Function::Create(llvm_function_type, llvm::Function::ExternalLinkage, plan_function.name.to_c_string(r.get_page()), module);
+        auto function_name = plan_function.name.to_c_string(r.get_page());
+        llvm::Function* llvm_function = llvm::Function::Create(llvm_function_type, llvm::Function::ExternalLinkage, function_name, module);
 
         HashMapBuilder<String, llvm::Value*>& argument_values_builder = *new (alignof(HashMapBuilder<String, llvm::Value>*), r.get_page()) HashMapBuilder<String, llvm::Value*>();
         _argument_iterator = plan_function.input.get_iterator();
@@ -128,21 +133,28 @@ namespace generator {
             }
         }
 
+        IRBuilder<> builder(context);
+        auto subprogram = di_builder.createFunction(di_file, function_name, StringRef(), di_file, 1,
+        di_builder.createSubroutineType(di_builder.getOrCreateTypeArray({})),
+        1, DINode::FlagPrototyped, DISubprogram::SPFlagDefinition);
         auto argument_values_map = HashMap<String, llvm::Value*>(r.get_page(), argument_values_builder);
         auto _block_iterator = plan_function.blocks.get_iterator();
         while (auto _block = _block_iterator.next()) {
             auto block = *_block;
             llvm::BasicBlock* basicBlock = llvm::BasicBlock::Create(context, block.name.to_c_string(r.get_page()), llvm_function);
-            IRBuilder<> Builder(context);
-            Builder.SetInsertPoint(basicBlock);
+            builder.SetInsertPoint(basicBlock);
+            builder.SetCurrentDebugLocation(DebugLoc(DILocation::get(context, 2, 0, subprogram)));
 
             HashMapBuilder<String, llvm::Value*>& block_values_builder = *new (alignof(HashMapBuilder<String, llvm::Value*>), r.get_page()) HashMapBuilder<String, llvm::Value*>();
             auto _instruction_iterator = block.instructions.get_iterator();
             while (auto _instruction = _instruction_iterator.next()) {
                 auto instruction = *_instruction;
-                add_instruction(Builder, argument_values_map, block_values_builder, instruction);
+                add_instruction(builder, argument_values_map, block_values_builder, instruction);
             }
         }
+
+
+        llvm_function->setSubprogram(subprogram);
 
         return llvm_function;
     }
@@ -151,13 +163,18 @@ namespace generator {
         Region _r;
         auto context = std::make_unique<LLVMContext>();
         auto module = std::make_unique<llvm::Module>(planModule.name.to_c_string(_r.get_page()), *context);
-        IRBuilder<> Builder(*context);
+
+        DIBuilder di_builder(*module);
+        DIFile* debug_file = di_builder.createFile("test.scaly", ".");
+        auto cu = di_builder.createCompileUnit(dwarf::DW_LANG_C, debug_file, "LLVM JIT Example", false, "", 0);
 
         auto _function_iterator = planModule.functions.get_iterator();
         while (auto _function = _function_iterator.next()) {
             auto planFunction = *_function;
-            auto function = generate_function(*module, planFunction);
-        }        
+            auto function = generate_function(*module, debug_file, di_builder, planFunction);
+        }
+
+        di_builder.finalize();
 
         if (llvm::verifyModule(*module, &llvm::errs())) {
                 llvm::errs() << "Error: Invalid module!\n";
@@ -169,18 +186,35 @@ namespace generator {
         InitializeNativeTarget();
         InitializeNativeTargetAsmPrinter();
 
-        auto jitter = orc::LLJITBuilder().create();
+        ExitOnError ExitOnErr;
+        auto jitter = ExitOnErr(LLJITBuilder()
+        // .setJITTargetMachineBuilder(
+        //     ExitOnErr(JITTargetMachineBuilder::detectHost()))
+        // .setObjectLinkingLayerCreator(
+        //     [&](ExecutionSession &ES, const Triple &TT) {
+        //         auto ObjLinkingLayer = 
+        //             std::make_unique<ObjectLinkingLayer>(ES);
+                
+        //         // Correct debug support for LLVM 19
+        //         auto DebugObjectManager = 
+        //             ExitOnErr(DebugObjectManagerPlugin::Create(TT));
+        //         ObjLinkingLayer->addPlugin(std::make_unique<DebugObjectManagerPlugin>(
+        //             std::move(DebugObjectManager)));
+                
+        //         return ObjLinkingLayer;
+        //     })
+        .create());
         if (!jitter) {
-            errs() << "Failed to create jitter: " << toString(jitter.takeError()) << "\n";
+            errs() << "Failed to create jitter\n";
             exit(1);
         }
 
-        if (auto Err = (*jitter)->addIRModule(orc::ThreadSafeModule(std::move(module), std::move(context)))) {
+        if (auto Err = jitter->addIRModule(orc::ThreadSafeModule(std::move(module), std::move(context)))) {
             errs() << "Failed to add IR module: " << Err << "\n";
             exit(1);
         }
 
-        auto main_sym = (*jitter)->lookup("main");
+        auto main_sym = jitter->lookup("main");
         if (!main_sym) {
             errs() << "Failed to lookup 'main'\n";
             exit(1);
