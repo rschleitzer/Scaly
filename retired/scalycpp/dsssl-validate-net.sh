bc294c7440e0a9c35d8745e8803cbb961c3b1859
ralf <ralf@scaly.io>
2025-12-06 17:29:59 +0100

    validation scripts added and updated

---
+++ retired/scalycpp/dsssl-validate-net.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+dotnet run --project /Users/r.schleitzer/repos/openjade-net/src/Nsgmls -- codegen/scaly.dsl > codegen/scaly.esis 2>&1
--- retired/scalycpp/validate-net.sh
+++ retired/scalycpp/validate-net.sh
@@ -1,4 +1,2 @@
 #!/bin/sh
-NSGMLS="dotnet run --project /Users/r.schleitzer/repos/openjade-net/src/Nsgmls/Nsgmls.csproj --"
-$NSGMLS scaly.sgm > scaly-net.esis
-diff scaly.esis scaly-net.esis
+dotnet run --project /Users/r.schleitzer/repos/openjade-net/src/Nsgmls/Nsgmls.csproj -- scaly.sgm > scaly.esis
