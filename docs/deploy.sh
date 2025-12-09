4c80ce1b4a59720c800bf8b30d751a8c849a78a6
ralf <ralf@scaly.io>
2025-12-09 09:53:26 +0100

    Add CloudFront invalidation to deploy script

--- docs/deploy.sh
+++ docs/deploy.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-# Deploy website to S3
+# Deploy website to S3 and invalidate CloudFront cache
 aws s3 sync website s3://scaly.io/ --delete
+aws cloudfront create-invalidation --distribution-id E3INKQI1B221G9 --paths "/*"
