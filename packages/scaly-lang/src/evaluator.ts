e77b11b50e8d59e8da9a54ad91b599f420a9e070
ralf <ralf@scaly.io>
2025-12-09 16:16:23 +0100

    Fix __dirname for ES modules in evaluator

--- packages/scaly-lang/src/evaluator.ts
+++ packages/scaly-lang/src/evaluator.ts
@@ -4,6 +4,10 @@ import { parseAndModel } from './modeler.js'
 import { Runtime, Value } from './runtime.js'
 import { formatModelError } from './model-error.js'
 import * as path from 'path'
+import { fileURLToPath } from 'url'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 
 export type EvalResult =
   | { _tag: 'Ok'; value: unknown; code: string }
